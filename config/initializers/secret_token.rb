dcf85991375839b1ad545a5d2ab1816666053b56
markpeace <mark@learnsomestuff.com>
2012-10-30 19:57:10 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Littlehoshi::Application.config.secret_token = 'c221c12049f5e44cdcd9fb08fa76d0d4f6831528d0fd685e032343d9850e361ebd79289af9eccd16e5e3ed574f05747285182b12ccf797bc0c2ca13c0e547cc6'
