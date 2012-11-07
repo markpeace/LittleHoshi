4c35bc1f1d1c8eac09053d3aab1fc93b9a12d306
markpeace <mark@learnsomestuff.com>
2012-11-07 07:33:17 +0000

    session controller working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-	@user=User.new(params[:user]).authenticate
-	#@user.authenticate
-	render :new
+	@user=User.new(params[:user])
+	if u=User.authenticate(@user.email, @user.password) then
+		redirect_to root_path
+	else
+		@user.errors.add(:email, "Sorry, your login details seem to be wrong")
+		render :new
+	end
   end
 
   def destroy
